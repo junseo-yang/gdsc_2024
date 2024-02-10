13fd4f5ea209a4d85453fb6676e97e566f8b57ce
gtitJ <junewon2020@gmail.com>
2024-02-10 16:12:52 -0500

    Added two buttons and one input to the  fragment_home.xml.

--- app/src/main/java/com/example/pa/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pa/ui/home/HomeFragment.kt
@@ -1,13 +1,20 @@
 package com.example.pa.ui.home
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.pa.databinding.FragmentHomeBinding
+import com.example.pa.ui.gallery.GalleryFragment
+
 
 class HomeFragment : Fragment() {
 
@@ -31,6 +38,18 @@ class HomeFragment : Fragment() {
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+
+        val buttonNotPaid: Button = binding.btnNotPaid
+        buttonNotPaid.setOnClickListener {
+            // Code here executes on main thread after user presses button
+            // TODO：come back
+            findNavController().navigate(com.example.pa.R.id.fragment_gallery)
+        }
+        val buttonPaid: Button = binding.btnNotPaid
+        buttonPaid.setOnClickListener {
+            // Code here executes on main thread after user presses button
+            // TODO：come back
         }
         return root
     }
