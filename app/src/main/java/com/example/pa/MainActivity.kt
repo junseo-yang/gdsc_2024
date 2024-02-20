b91ee37d507634d9f3efbdcaa423adfb7fefb0f6
Maísa Wolff Resplande <maisawres@gmail.com>
2024-02-19 20:45:50 -0500

    Update navigation.

--- app/src/main/java/com/example/pa/MainActivity.kt
+++ app/src/main/java/com/example/pa/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_create_manually, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_create_manually, R.id.nav_aiGenerate
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
